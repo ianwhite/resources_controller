212ab46007d89c3b2d14dd9f07cb88bf0665701e
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-01-31 11:31:25 +0000

    Documentation fro resources_controller
    git-svn-id: https://svn.ardes.com/rails_plugins/resources_controller@161 845bbffb-5c18-0410-91b3-f25c072b94c1

--- lib/ardes/resources_controller.rb
+++ lib/ardes/resources_controller.rb
@@ -1,9 +1,11 @@
 module Ardes
   module ResourcesController
-    # Specifies that this controller is a REST style controller for the named resource (a resources - like :users)
+    # Specifies that this controller is a REST style controller for the named resource (a resources plural like :users).  You can specify that the 
+    # resource is a nested resource.
     #
     # Options:
     # * <tt>:class_name:</tt> The class name of the resource, if it can't be inferred from its name
+    # * <tt>:collection_name:</tt> (if using nested resources - see nested_in) The collection that the resources belongs to, if it can't be inferred from its name
     # * <tt>:route_name:</tt> The name of the route (of the resources, i.e. :users), if it can't be inferred from the name of the controller
     # * <tt>:in:</tt> Ordered array of singular model names which correspond to nesting a resource. 
     #
@@ -20,11 +22,11 @@ module Ardes
     # model to find and create the resources.  See nested_in for more details on this, and customising the default behaviour
     #
     def resources_controller_for(resources_name, options = {})
-      options.assert_valid_keys(:class_name, :route_name, :in)
+      options.assert_valid_keys(:class_name, :collection_name, :route_name, :in)
       
       self.class_eval do
         unless included_modules.include?(::Ardes::ResourcesController::InstanceMethods)
-          class_inheritable_accessor :resources_name, :resource_name, :resource_class, :route_name, :enclosing_resource_names
+          class_inheritable_accessor :resources_name, :resource_name, :resource_class, :resource_collection_name, :route_name, :enclosing_resource_names
           self.enclosing_resource_names ||= []
           include InstanceMethods
           include ActionMethods
@@ -32,13 +34,65 @@ module Ardes
       end
       
       self.resources_name = resources_name.to_s
-      self.resource_name    = self.resources_name.singularize
-      self.route_name      = options[:route_name] || controller_name
-      self.resource_class   = (options[:class_name] || self.resource_name.classify).constantize
+      self.resource_name  = self.resources_name.singularize
+      self.route_name     = options[:route_name] || controller_name
+      self.resource_class = (options[:class_name] || self.resource_name.classify).constantize
+      self.resource_collection_name = options[:collection_name]
       
       nested_in(*options[:in]) if options[:in]
     end
     
+    # [This can also be called via the <tt>:in</tt> option in <tt>resources_controller_for</tt>]
+    #
+    # Specifies that the resource is nested in another resource.  Can be called in two ways:
+    # * single: <tt>nested_in :resource[, options] [&block]</tt>
+    # * multiple: <tt>nested_in :resource1[, :resource2]...</tt>
+    #
+    # The multiple version is equivalent to calling <tt>nested_in</tt> multiple times, and can only be used to provide the default functionality.
+    #
+    # Calling <tt>nested_in :foo</tt> will result in the following:
+    # * accessor set up for :foo, which gets and sets @foo
+    # * a before_filter which sets @foo according to params[:foo_id]
+    # * resource_service will change from being the resource class to being @foo.resources association
+    #
+    # You can customise how the before_filter sets @foo by passing a block:
+    #  
+    #  resource_controller_for :posts
+    #  nested_in :forum {|controller| controller.forum = Forum.find(controller.params[:forum_id])}
+    #
+    # (The above block just happens to be the default behaviour of nested_in)
+    #
+    # === Multiple <tt>nested_in</tt>s
+    # A typical resource that is multiply nested is found in the following way (example is forum has_many posts, each has_many comments):
+    # 
+    #  @forum = Forum.find(params[:forum_id])
+    #  @post = @forum.posts.fiind(params[:post_id])
+    #  @comment = @post.comments.find(params[:id])
+    #  @comments = @post.comments.find(:all)
+    #
+    # The first nesting resource is found from the class, and subsequent ones are found from a collection.  Because of this the first, and subsequent
+    # calls to nested_in have different options.
+    #
+    # First <tt>nested_in</tt> options
+    #  * <tt>:class_name:</tt> the name of the class (e.g. 'Forum' in the above example) if it can't be inferred from the name
+    #
+    # Subsequent <tt>nested_in</tt> options
+    #  * <tt>:collection_name:</tt> the name of the collection (e.g. 'posts' in the above example) if it can't be inferred from the name
+    #
+    # Example using options (models are in Funky:: module, and collections are name 'the_*'):
+    #  
+    #  class CommentsController < ApplicationController
+    #    resources_controller_for :comments, :collection_name => 'the_comments' # @post.the_comments will be used as the resource_service
+    #    nested_in :forum, :class_name => 'Funky::Forum'                        # => Funky::Forum will be used to find @forum
+    #    nested_in :posts, :collection_name => 'the_posts'                      # => @forum.the_posts will be used to find @post
+    #  end
+    #
+    # === Resource naming
+    # However, if your models are named according to the usual conventions, it will just work.
+    #
+    #  class CommentsController < ApplicationController
+    #    resources_controller_for :comments, :in => [:forum, :post]
+    #  end
     def nested_in(*names, &block)
       options = names.last.is_a?(Hash) ? names.pop : {}
       raise ArgumentError "when giving more than one nesting, you may not specify options or a block" if names.length > 1 and (block_given? or options.length > 0)
@@ -59,7 +113,7 @@ module Ardes
       
       class_eval do
         define_method :resource_service do
-          @resource_service ||= send(enclosing_resource_names.last).send(resources_name)
+          @resource_service ||= send(enclosing_resource_names.last).send(resource_collection_name || resources_name)
         end
       end
       
@@ -72,12 +126,12 @@ module Ardes
     end
     
     def add_subsequent_enclosing(name, options = {}, &block)
-      options.assert_valid_keys(:resources_name)
+      options.assert_valid_keys(:collection_name)
       
       if block_given?
         before_filter(&block)
       else
-        prev, coll, fk = enclosing_resource_names.last, options[:resources_name] || name.pluralize, name.foreign_key
+        prev, coll, fk = enclosing_resource_names.last, options[:collection_name] || name.pluralize, name.foreign_key
         before_filter {|c| c.send("#{name}=", c.send(prev).send(coll).find(c.params[fk]))}
       end
     end
@@ -167,51 +221,73 @@ module Ardes
         base.send :hide_action, *instance_methods
       end
       
+      # returns the controller's current resource
       def resource
         instance_variable_get("@#{resource_name}")
       end
       
+      # sets the controller's current resource
       def resource=(elem)
         instance_variable_set("@#{resource_name}", elem)
       end
   
+      # returns the controller's current resources collection
       def resources
         instance_variable_get("@#{resources_name}")
       end
       
+      # sets the controller's current resource collection
       def resources=(coll)
         instance_variable_set("@#{resources_name}", coll)
       end
       
+      # returns an array of the controller's enclosing (nested in) resources
       def enclosing_resources
         @enclosing_resources ||= enclosing_resource_names.inject([]){|m, name| m << send(name)}
       end
       
+      # returns the current resource service.  This is used to find and create resources (see find_resource, find_resources, new_resource)
+      #
+      # For a resource which is not nested this will be the resource class.
+      #
+      # For a nested resource this will be a has_many association
       def resource_service
         @resource_service ||= resource_class
       end
       
+      # sets the current resource service.
+      #
+      # If you wish to provide a customised resource service you need to make sure the object responds
+      # appropriately to:
+      # * find :all
+      # * find id
+      # * new
       def resource_service=(service)
         @resource_service = service
       end
       
-    protected  
+    protected
+      # finds the collection of resources
       def find_resources()
         resource_service.find :all
       end
   
+      # finds the resource, using the passed id (default is params[:id])
       def find_resource(id = params[:id])
         resource_service.find id
       end
       
+      # makes a new resource, using the passed hash (default is params[resource_name])
       def new_resource(attrs = params[resource_name])
         resource_service.new(attrs)
       end
       
-      def resource_url
+      # returns the url for the passed resource (default is self.resource)
+      def resource_url(resource = self.resource)
         send("#{route_name.singularize}_url", *(enclosing_resources + [resource]))
       end
   
+      # returns the url for the resources collection
       def resources_url
         send("#{route_name}_url", *enclosing_resources)
       end
