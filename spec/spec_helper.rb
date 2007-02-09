8de3a009f82c36c3e53fe6ea62ff672351d83b28
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-02-09 11:55:31 +0000

    git-svn-id: https://svn.ardes.com/rails_plugins/resources_controller@177 845bbffb-5c18-0410-91b3-f25c072b94c1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.join(File.dirname(__FILE__), "../../../../config/environment.rb"))
 require 'spec/rails'
 
+config = YAML::load(IO.read(File.join(File.dirname(__FILE__), 'database.yml')))
+ActiveRecord::Base.establish_connection(config['db'])
+
 # Even if you're using RSpec, RSpec on Rails is reusing some of the
 # Rails-specific extensions for fixtures and stubbed requests, response
 # and other things (via RSpec's inherit mechanism). These extensions are 
@@ -23,6 +26,21 @@ end
 
 
 # Testing app setup
+
+ActiveRecord::Migration.suppress_messages do
+  ActiveRecord::Schema.define(:version => 0) do
+    create_table :forums, :force => true do |t|
+    end
+
+    create_table :posts, :force => true do |t|
+      t.column "forum_id", :integer
+    end
+
+    create_table :comments, :force => true do |t|
+      t.column "post_id", :integer
+    end
+  end
+end
 
 #
 # Usage with rails compliants naming - default config should just work
