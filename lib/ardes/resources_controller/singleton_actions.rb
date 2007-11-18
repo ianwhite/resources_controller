80ddcd0bb13a43ba8fb28e0c58b60341b13438c9
ian <ian@845bbffb-5c18-0410-91b3-f25c072b94c1>
2007-11-18 11:51:58 +0000

    resources_controller: simplified SingletonActions module
    git-svn-id: https://svn.ardes.com/rails_plugins/resources_controller@503 845bbffb-5c18-0410-91b3-f25c072b94c1

--- lib/ardes/resources_controller/singleton_actions.rb
+++ lib/ardes/resources_controller/singleton_actions.rb
@@ -1,81 +1,9 @@
 module Ardes#:nodoc:
   module ResourcesController
     module SingletonActions
-      # GET /event
-      # GET /event
-      def show
-        self.resource = find_resource
-
-        respond_to do |format|
-          format.html # show.erb.html
-          format.js
-          format.xml  { render :xml => resource }
-        end
-      end
-
-      # GET /event/new
-      def new
-        self.resource = new_resource
-
-        respond_to do |format|
-          format.html # new.html.erb
-          format.js
-          format.xml  { render :xml => resource }
-        end
-      end
-
-      # GET /event/edit
-      def edit
-        self.resource = find_resource
-
-        respond_to do |format|
-          format.html # edit.html.erb
-          format.js
-          format.xml  { render :xml => resource }
-        end
-      end
-
-      # POST /event
-      # POST /event.xml
-      def create
-        self.resource = new_resource
-
-        respond_to do |format|
-          if resource.save
-            format.html do
-              flash[:notice] = "#{resource_name.humanize} was successfully created."
-              redirect_to resource_url
-            end
-            format.js
-            format.xml  { render :xml => resource, :status => :created, :location => resource_url }
-          else
-            format.html { render :action => "new" }
-            format.js   { render :action => "new" }
-            format.xml  { render :xml => resource.errors, :status => :unprocessable_entity }
-          end
-        end
-      end
-
-      # PUT /event
-      # PUT /event.xml
-      def update
-        self.resource = find_resource
-
-        respond_to do |format|
-          if resource.update_attributes(params[resource_name])
-            format.html do
-              flash[:notice] = "#{resource_name.humanize} was successfully updated."
-              redirect_to resource_url
-            end
-            format.js
-            format.xml  { head :ok }
-          else
-            format.html { render :action => "edit" }
-            format.js   { render :action => "edit" }
-            format.xml  { render :xml => resource.errors, :status => :unprocessable_entity }
-          end
-        end
-      end
+      include Actions
+      
+      undef index
 
       # DELETE /event
       # DELETE /event.xml
