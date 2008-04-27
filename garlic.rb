65e0b588592978763527ccc9d77e89c1ba5937b1
Ian White <ian.w.white@gmail.com>
2008-04-27 13:02:44 +0100

    Updating garlic config

--- garlic.rb
+++ garlic.rb
@@ -13,7 +13,7 @@ garlic do
   repo 'rails', :url => 'git://github.com/rails/rails'#, :local => "~/dev/vendor/rails"
   repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'#, :local => "~/dev/vendor/rspec"
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'#, :local => "~/dev/vendor/rspec-rails"
-  repo 'resources_controller', :url => '.'
+  repo 'resources_controller', :path => '.'
 
   target 'edge'
   target '2.0-stable', :branch => 'origin/2-0-stable'
@@ -21,7 +21,7 @@ garlic do
 
   all_targets do
     prepare do
-      plugin 'resources_controller'
+      plugin 'resources_controller', :clone => true
       plugin 'rspec'
       plugin('rspec-rails') { sh "script/generate rspec -f" }
     end
