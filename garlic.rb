8895b98c10cf16df65bff2a73f18bd656305401f
Ian White <ian.w.white@gmail.com>
2008-11-25 17:00:07 +1100

    Updated garlic.rb to 0.1.5 API (no all_targets)

--- garlic.rb
+++ garlic.rb
@@ -1,30 +1,28 @@
 garlic do
   repo 'resources_controller', :path => '.'
 
-  repo 'rails', :url => 'git://github.com/rails/rails'#, :local => "~/dev/vendor/rails"
-
+  repo 'rails', :url => 'git://github.com/rails/rails'
   # using ianwhite/rspec-rails as it has some patches that are not yet applied in dchelimsky/rspec-rails
   repo 'ianwhite-rspec', :url => 'git://github.com/ianwhite/rspec'
   repo 'ianwhite-rspec-rails', :url => 'git://github.com/ianwhite/rspec-rails'
 
-  #target 'edge'
-  target '2.0-stable', :branch => 'origin/2-0-stable'
-  target '2.1-stable', :branch => 'origin/2-1-stable'
-  target '2.2-stable', :branch => 'origin/2-2-stable'
+  ['origin/2-0-stable', 'origin/2-1-stable', 'origin/2-2-stable'].each do |rails|
 
-  all_targets do
-    prepare do
-      plugin 'resources_controller', :clone => true
-      plugin 'ianwhite-rspec', :as => 'rspec'
-      plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
-        sh "script/generate rspec -f"
+    target "Rails: #{rails}", :tree_ish => rails do
+      prepare do
+        plugin 'resources_controller', :clone => true
+        plugin 'ianwhite-rspec', :as => 'rspec'
+        plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
+          sh "script/generate rspec -f"
+        end
       end
-    end
   
-    run do
-      cd "vendor/plugins/resources_controller" do
-        sh "rake spec:rcov:verify && rake spec:generate"
+      run do
+        cd "vendor/plugins/resources_controller" do
+          sh "rake spec:rcov:verify && rake spec:generate"
+        end
       end
     end
+
   end
 end
