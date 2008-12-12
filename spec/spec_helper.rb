4a26949fd713a05e5a4b0c1f5a7a8def0fe5ea9d
Ian White <ian.w.white@gmail.com>
2008-12-12 13:44:40 +1100

    Removed jiggery-pokey since #1557 on rails has been resolved

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,5 @@
 ENV["RAILS_ENV"] ||= "test"
-
-# this jiggery pokery is because edge rails currently borks unless
-# you load config/environment from RAILS_ROOT - see #1557
-require 'fileutils'
-include FileUtils
-cd File.join(File.dirname(__FILE__), "../../../..") do
-  require "config/environment"
-end
-
+require File.join(File.dirname(__FILE__), "../../../../config/environment")
 require 'spec'
 require 'spec/rails'
 
