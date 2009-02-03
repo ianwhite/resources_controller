80bf48d894ccc3aebb878619daef4d658afd22a7
Ian White <ian.w.white@gmail.com>
2009-02-03 10:05:34 +0000

    Updated garlic and made notes about 2.x compat

--- garlic.rb
+++ garlic.rb
@@ -1,18 +1,16 @@
 garlic do
   repo 'resources_controller', :path => '.'
-
   repo 'rails', :url => 'git://github.com/rails/rails'
-  # using ianwhite/rspec-rails as it has some patches that are not yet applied in dchelimsky/rspec-rails
-  repo 'ianwhite-rspec', :url => 'git://github.com/ianwhite/rspec'
-  repo 'ianwhite-rspec-rails', :url => 'git://github.com/ianwhite/rspec-rails'
+  repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'
+  repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'
 
-  ['origin/master', 'origin/2-0-stable', 'origin/2-1-stable', 'origin/2-2-stable'].each do |rails|
+  ['origin/2-1-stable', 'origin/2-2-stable'].each do |rails|
 
     target "Rails: #{rails}", :tree_ish => rails do
       prepare do
         plugin 'resources_controller', :clone => true
-        plugin 'ianwhite-rspec', :as => 'rspec'
-        plugin 'ianwhite-rspec-rails', :as => 'rspec-rails' do
+        plugin 'rspec', :as => 'rspec'
+        plugin 'rspec-rails', :as => 'rspec-rails' do
           sh "script/generate rspec -f"
         end
       end
