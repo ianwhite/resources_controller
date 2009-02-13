0736202d7ff508fed40bbb186fd774ed7e3b44ad
Ian White <ian.w.white@gmail.com>
2009-02-13 12:59:51 +0000

    add all 2.x branches to CI

--- garlic.rb
+++ garlic.rb
@@ -4,13 +4,13 @@ garlic do
   repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'
 
-  ['origin/2-2-stable', 'origin/2-1-stable'].each do |rails|
+  ['master', '2-2-stable', '2-1-stable', '2-0-stable'].each do |rails|
 
-    target "Rails: #{rails}", :tree_ish => rails do
+    target rails, :tree_ish => "origin/#{rails}" do
       prepare do
         plugin 'resources_controller', :clone => true
-        plugin 'rspec', :as => 'rspec'
-        plugin 'rspec-rails', :as => 'rspec-rails' do
+        plugin 'rspec'
+        plugin 'rspec-rails' do
           sh "script/generate rspec -f"
         end
       end
