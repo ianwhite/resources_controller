8f64d1c040edc896e65795f3d328f4924c2994b7
Ian White <ian.w.white@gmail.com>
2009-06-27 18:01:08 +0100

    BC fix for running specs on ruby 1.8.7 + rails 2.0.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,8 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
 end
+
+# BC: fix for ruby 1.8.7 + rails 2.0.x Enumerable problem
+unless String.new.respond_to?(:force_encoding)
+  String.send(:remove_method, :chars) rescue nil
+end
