1ce84cdba19f41f258b6a16a44924bab10172173
Ian White <ian.w.white@gmail.com>
2009-09-19 17:56:10 +0100

    Spec'd and implemented #nesting_segments

--- lib/ardes/resources_controller/request_path_introspection.rb
+++ lib/ardes/resources_controller/request_path_introspection.rb
@@ -8,16 +8,47 @@ module Ardes
       end
       
       def nesting_request_path
-        @nesting_request_path ||= remove_namespace(request_path.sub(%r(/#{resource_segment}(?!.*/#{resource_segment}).*$), ''))
+        @nesting_request_path ||= remove_namespace(request_path.sub(%r(/#{current_segment}(?!.*/#{current_segment}).*$), ''))
+      end
+      
+      # returns an array of hashes like {:segment => 'forum', :singleton => false}
+      def nesting_segments
+        segments_for_path_and_keys(nesting_request_path, params.keys.select{|k| k.to_s[-3..-1] == '_id'})
       end
       
     private
-      def resource_segment
+      def current_segment
         respond_to?(:resource_specification) ? resource_specification.segment : controller_name
       end
         
       def remove_namespace(path)
         (controller_path != controller_name) ? path.sub(%r(^/#{controller_path.sub(%r(/#{controller_name}), '')}), '') : path
+      end
+    
+      def segments_for_path_and_keys(path, keys)
+        key_segments = keys.map{|k| segment_for_key(k)}
+        path_segments = path[1..-1].split('/')
+        segments = []
+        while path_segments.any? do
+          segment = path_segments.shift
+          if key_segments.include?(segment)
+            segments << {:segment => segment, :singleton => false}
+            path_segments.shift # swallow following :id
+          else
+            segments << {:segment => segment, :singleton => true}
+          end
+        end
+        segments
+      end
+      
+      def segment_for_key(key)
+        if respond_to?(:specifications) && spec = specifications.find{|s| s.respond_to?(:key) && s.key == key.to_s}
+          spec.segment
+        elsif spec = resource_specification_map.values.find{|s| s.key == key.to_s}
+          spec.segment
+        else
+          key.to_s[0..-4].pluralize
+        end
       end
     end
   end
--- spec/lib/request_path_introspection_spec.rb
+++ spec/lib/request_path_introspection_spec.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 module RequestPathIntrospectionSpec
+  class Forum < ActiveRecord::Base; end
+  
+  class User < ActiveRecord::Base; end
+  
   describe "RequestPathIntrospection" do
     before do
       @klass = Class.new(ActionController::Base)
@@ -28,6 +32,12 @@ module RequestPathIntrospectionSpec
         @controller.send(:nesting_request_path).should == '/users/1'
       end
       
+      it "when resource_specification present, whould remove taht segment" do
+        @controller.stub!(:resource_specification).and_return(Ardes::ResourcesController::Specification.new(:forum, :class => RequestPathIntrospectionSpec::Forum, :segment => 'foromas'))
+        @controller.stub!(:request_path).and_return('/users/1/foromas/2')
+        @controller.send(:nesting_request_path).should == '/users/1'
+      end
+      
       it "should remove only the controller_name segment, when nesting is same name" do
         @controller.stub!(:request_path).and_return('/forums/1/forums/2')
         @controller.send(:nesting_request_path).should == '/forums/1'
@@ -37,6 +47,47 @@ module RequestPathIntrospectionSpec
         @controller.stub!(:controller_path).and_return('some/name/space/forums')
         @controller.stub!(:request_path).and_return('/some/name/space/users/1/secret/forums')
         @controller.send(:nesting_request_path).should == '/users/1/secret'
+      end
+    end
+    
+    describe "#nesting_segments" do
+      describe "when params include :user_id" do
+        before do
+          @controller.params[:user_id] = '1'
+        end
+        
+        it "and request path is '/users/1/forums', should return [{:segment => 'users', :singleton => false}]" do
+          @controller.request.stub!(:path).and_return('/users/1/forums')
+          @controller.send(:nesting_segments).should == [{:segment => 'users', :singleton => false}]
+        end
+        
+        it "and request path is '/account/users/1/forums', should return [{:segment => 'account', :singleton => true}, {:segment => 'users', :singleton => false}]" do
+          @controller.request.stub!(:path).and_return('/account/users/1/forums')
+          @controller.send(:nesting_segments).should == [{:segment => 'account', :singleton => true}, {:segment => 'users', :singleton => false}]
+        end
+        
+        describe "when controller has nesting for :user => 'muchachos'" do
+          before do
+            @klass.resources_controller_for :forums, :class => RequestPathIntrospectionSpec::Forum
+            @klass.nested_in :user, :segment => "muchachos", :class => RequestPathIntrospectionSpec::User
+          end
+          
+          it "and request path is '/muchachos/1/forums', should return [{:segment => 'muchachos', :singleton => false}]" do
+            @controller.request.stub!(:path).and_return('/muchachos/1/forums')
+            @controller.send(:nesting_segments).should == [{:segment => 'muchachos', :singleton => false}]
+          end
+        end
+        
+        describe "when enclosing reosurce has mapping for :user => 'muchachos'" do
+          before do
+            @klass.map_enclosing_resource :user, :segment => "muchachos", :class => RequestPathIntrospectionSpec::User
+          end
+          
+          it "and request path is '/muchachos/1/forums', should return [{:segment => 'muchachos', :singleton => false}]" do
+            @controller.request.stub!(:path).and_return('/muchachos/1/forums')
+            @controller.send(:nesting_segments).should == [{:segment => 'muchachos', :singleton => false}]
+          end
+        end
       end
     end
   end
