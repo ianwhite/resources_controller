96db1614741bf3efae0d1628767943edd73ee3b2
Ian White <ian.w.white@gmail.com>
2009-09-19 16:42:47 +0100

    Added RequestPathIntrospection module and specs, for inferrng wildcard resources without recourse to looking routes

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 require 'ardes/resources_controller'
 ActionController::Base.extend Ardes::ResourcesController
+ActionController::Base.send :include, Ardes::ResourcesController::RequestPathIntrospection
 
 require 'ardes/active_record/saved'
 ActiveRecord::Base.send :include, Ardes::ActiveRecord::Saved
