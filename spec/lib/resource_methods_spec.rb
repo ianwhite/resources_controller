b5fbbb9b2aa5e94fe6c949aa0ee787311e2496e9
Ian White <ian.w.white@gmail.com>
2009-12-20 09:20:29 +0000

    Added spec for new_resource accepting block syntax

---
+++ spec/lib/resource_methods_spec.rb
@@ -0,0 +1,21 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../app'))
+
+module ResourceMethodsSpec
+  class MyController < ActionController::Base
+    resources_controller_for :users
+  end
+  
+  describe "#new_resource" do
+    it "should accept block syntax" do
+      c = MyController.new
+      c.resource_service = User
+      c.stub!(:params).and_return({})
+      r = c.send(:new_resource) do |u|
+        u.login = "Fred"
+      end
+      r.should be_kind_of User
+      r.login.should == "Fred"
+    end
+  end
+end
