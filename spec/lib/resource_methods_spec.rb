6c080a74a1e05b4298894eb662a2b7ace8c6320b
Ian White <ian.w.white@gmail.com>
2009-12-20 09:35:31 +0000

    Added specs for resource_methods mixins

--- spec/lib/resource_methods_spec.rb
+++ spec/lib/resource_methods_spec.rb
@@ -6,6 +6,28 @@ module ResourceMethodsSpec
     resources_controller_for :users
   end
   
+  module MyResourceMethods
+  
+  protected
+    def new_resource(attrs = (params[resource_name] || {}), &block)
+      "my new_resource"
+    end
+    
+    def find_resource(id = params[:id])
+      "my find_resource"
+      
+    end
+    
+    def find_resources
+      "my find_resources"
+    end
+  end
+  
+  class MyControllerWithMyResourceMethods < ActionController::Base
+    resources_controller_for :users
+    include MyResourceMethods
+  end
+  
   if Rails.respond_to?(:version) && Rails.version >= "2.3"
     describe "#new_resource" do
       it "should accept block syntax" do
@@ -18,6 +40,25 @@ module ResourceMethodsSpec
         r.should be_kind_of User
         r.login.should == "Fred"
       end
+    end
+  end
+  
+  describe "A controller with resource methods mixed in after resources_controller_for" do
+    before do
+      @controller = MyControllerWithMyResourceMethods.new
+      @controller.resource_service = User
+    end
+    
+    it "#new_resource should call MyResourceMethods#new_resource" do
+      @controller.send(:new_resource, {}).should == 'my new_resource'
+    end
+
+    it "#find_resource should call MyResourceMethods#find_resource" do
+      @controller.send(:find_resource, 1).should == 'my find_resource'
+    end
+
+    it "#find_resources should call MyResourceMethods#new_resource" do
+      @controller.send(:find_resources).should == 'my find_resources'
     end
   end
 end
