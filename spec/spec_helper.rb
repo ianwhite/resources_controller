b369a55c81d1bdfda7b495e2bee80ea7c0052a8c
Andrew Bloomgarden <stalkingtiger@gmail.com>
2010-09-07 23:18:53 -0700

    Empty view files to deal with RSpec 2 & Rails 3's requirement for them.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require File.join(File.dirname(__FILE__), "../../../../config/environment")
 require 'rspec'
 require 'rspec/rails'
 
+# RSpec 2 doesn't fight Rails's requiring a template for rendering,
+# even in controller specs, so we need to provide empty ones in order to not have
+# just about every test fail.
+ActionController::Base.view_paths = [File.join(File.dirname(__FILE__), "app/views")]
+
 RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
